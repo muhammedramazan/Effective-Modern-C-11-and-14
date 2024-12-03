6f3b2e9030902be2a87bd3cc123d94d006716e6a
muhammedramazan <muhammedramazankilic@gmail.com>
2024-12-03 17:25:35 +0300

    item3 finish

--- 1- Deducing Types/item3.cpp
+++ 1- Deducing Types/item3.cpp
@@ -5,6 +5,9 @@ const int i = 0;
 decltype(i) i_dec{12};  //  is const int
 
 class Widget {};
+
+const Widget w__;
+const Widget& cw__{w__};
 bool f(const Widget& w) {
   decltype(w) w_dec{w};  // is const Widget&
   decltype(f) f_dec;     // is bool(const Widget&)
@@ -28,9 +31,44 @@ vector_temp<int> v_temp;
 decltype(v_temp) v_temp_dec;  // is vector_temp<int>
 
 std::vector<int> v_int;
-decltype(v_int[0]) v_int_dec{v_int[0]};  //int&
+decltype(v_int[0]) v_int_dec{v_int[0]};  // int&
 std::vector<bool> v_bool;
-decltype(v_bool[0]) v_bool_dec;  //std::vector<bool>::reference
+decltype(v_bool[0]) v_bool_dec;  // std::vector<bool>::reference
+
+// -- -- -- --
+// 1-)
+template <typename Container, typename Index>
+auto authAndAccess_auto(Container& c, Index i) -> auto {
+  return c[i];
+}
+// 2-)
+template <typename Container, typename Index>
+auto authAndAccess_trailing(Container& c, Index i) -> decltype(c[i]) {
+  return c[i];
+}
+// 3-)
+template <typename Container, typename Index>
+decltype(auto) authAndAccess_decltype_auto(Container& c, Index i) {
+  return c[i];
+}
+decltype(auto) w_dec_auto_ = w__;
+auto w_auto_ = (w__);
+decltype(auto) c_w_dec_auto_ = cw__;
+auto c_w_auto_ = (cw__);
+// 4-)
+template <typename Container, typename Index>
+auto authAndAccess_forward_trailing(Container&& c, Index i)
+    -> decltype(std::forward<Container>(c)[i]) {
+  return std::forward<Container>(c)[i];
+}
+// 5-)
+template <typename Container, typename Index>
+decltype(auto) authAndAccess_forward_dec_auto(Container&& c, Index i) {
+  return std::forward<Container>(c)[i];
+}
+// 6-)
+int x__t = 0;
+decltype((x__t)) x___x;
 int main(int argc, char const* argv[]) {
   /* code */
 
